42e8df719fed8f559e57723b5cefe068a8147986
zigamak <zigamakinde@gmail.com>
2022-04-19 22:38:16 +0100

    header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+//Components
+import Header from './components/Header';
 
+//styles
+import{ GlobalStyle} from './GlobalStyle';  
 function App() {
   return (
     <div className="App">
+      <Header/> 
       Start here.
+      <GlobalStyle/>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import RMDBLogo from '../../images/react/react-movie.logo.svg'
+import TMDBLogo from '../../images/react/tmdb_logo.svg'
+
+import {Wrapper, Content, LogoImg, TMDBLogoImg} from '.Header.styles';
+const Header = () => (
+    <Wrapper>
+        <Content>
+            <LogoImg src={RMDBLogo} alt='rmdb-logo'/>
+            <TMDBLogoImg src={TMDBLogo} alt='tmdb=logo' />
+        </Content>
+    </Wrapper>
+)
+
+export default Header;
