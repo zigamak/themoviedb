423bfb1905dc5b4aec816f8b6fc36ca970c938fc
zigamak <zigamakinde@gmail.com>
2022-04-20 21:35:23 +0100

    Home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // Components
 import Header from './components/Header';
+import Home from './components/Home';
 
 // Styles
 import { GlobalStyle } from './GlobalStyle';
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className='App'>
       <Header />
-      Start here.
+      <Home />
       <GlobalStyle />
     </div>
   );
--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,9 +1,31 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  background: var(--darkGrey);
+  padding: 0 20px;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: var(--maxWidth);
+  padding: 20px 0;
+  margin: 0 auto;
+`;
 
-export const LogoImg = styled.img``;
+export const LogoImg = styled.img`
+  width: 200px;
 
-export const TMDBLogoImg = styled.img``;
+  @media screen and (max-width: 500px) {
+    width: 150px;
+  }
+`;
+
+export const TMDBLogoImg = styled.img`
+  width: 100px;
+
+  @media screen and (max-width: 500px) {
+    width: 80px;
+  }
+`;
