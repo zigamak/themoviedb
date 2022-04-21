1c828ae1c515850a3ccd40a30bf48f88cc176c36
zigamak <zigamakinde@gmail.com>
2022-04-21 23:09:28 +0100

    Hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 
 // Hook
-
+import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const { state, loading, error } = useHomeFetch();
+
+  console.log(state);
 
   return <div>Home Page</div>;
 };
