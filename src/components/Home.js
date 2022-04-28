6edd30b7873eb5614d95823917d3bd0c6ee787be
zigamak <zigamakinde@gmail.com>
2022-04-28 22:24:44 +0100

    API Key

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+//API
+import API from '../API';
 // Config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
@@ -9,11 +11,26 @@ import { useHomeFetch } from '../hooks/useHomeFetch';
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const [state,setState]=useState();
+  const [loading,setLoading]= useState(false);
+  const [error, setError]= useState(false);
+  
+  const fetchMovies= async(page, searchTerm="")=>{
+    try{
+      setError(false);
+      setLoading(true);
+      const movies=await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+    } catch (error){
+      setError(true);
+    }
+  };
 
-  console.log(state);
-
+  //Initial Render
+  useEffect(()=>{
+    fetchMovies(1)
+    },[])
   return <div>Home Page</div>;
-};
+}
 
 export default Home;
