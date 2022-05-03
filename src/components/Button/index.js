d9dceb66adfec6db1b30079e07ba667d637ebc24
zigamak <zigamakinde@gmail.com>
2022-05-03 23:24:39 +0100

    Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//styles
+import {Wrapper} from './Button.styles';
+
+const Button = ({text, callback})=>(
+    <Wrapper type="button" onClick={callback} >
+        {text}
+    </Wrapper>
+);
+
+export default Button;
