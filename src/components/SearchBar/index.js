28e5da8dd03ca279b9665faecd36561faea665ff
zigamak <zigamakinde@gmail.com>
2022-05-03 23:25:23 +0100

    SearchBar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,34 +1,39 @@
-import React, {useState, useEffect, useRef} from "react";
-
-//Image
+import React, { useState, useEffect, useRef } from 'react';
+// Image
 import searchIcon from '../../images/search-icon.svg';
+// Styles
+import { Wrapper, Content } from './SearchBar.styles';
 
-//Styles
-import {Wrapper, Content} from './SearchBar.styles';
+const SearchBar = ({ setSearchTerm }) => {
+  const [state, setState] = useState('');
+  const initial = useRef(true);
 
-const SearchBar = ({setSearchTerm})=>{
- 
-    useEffect(()=> {
-        const timer =setTimeout(()=>{
-            setSearchTerm(state);
-        },500)
-        return()=> clearTimeout(timer)
-    },[setSearchTerm,state])
+  useEffect(() => {
+    if (initial.current) {
+      initial.current = false;
+      return;
+    }
 
-    const [state, setState]= useState('');
-    return (
-        <Wrapper>
-            <Content>
-                <img src={searchIcon} alt='search-icon' />
-                <input 
-                type='text' 
-                placeholder="Search Movies" 
-                onChange={event=>setState(event.currentTarget.value)}
-                value={state}
-                />
-            </Content>
-        </Wrapper>
-    )
-}
+    const timer = setTimeout(() => {
+      setSearchTerm(state);
+    }, 500)
+
+    return () => clearTimeout(timer)
+  },[setSearchTerm, state])
+
+  return (
+    <Wrapper>
+      <Content>
+        <img src={searchIcon} alt='search-icon' />
+        <input
+          type='text'
+          placeholder='Search Movie'
+          onChange={event => setState(event.currentTarget.value)}
+          value={state}
+        />
+      </Content>
+    </Wrapper>
+  );
+};
 
 export default SearchBar;
