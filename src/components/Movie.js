b93dd6fc5b4c9e00749369e6035a6d8c954e1b86
zigamak <zigamakinde@gmail.com>
2022-05-04 14:50:30 +0100

    Created the Actor Components for the movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -8,6 +8,7 @@ import Grid from './Grid';
 import Spinner from './Spinner';
 import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
+import Actor from './Actor';
 // Hook
 import { useMovieFetch } from '../hooks/useMovieFetch';
 // Image
@@ -25,7 +26,25 @@ const Movie = () => {
     <>
       <BreadCrumb movieTitle={movie.original_title} />
       <MovieInfo movie={movie} />
-      <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue}></MovieInfoBar>
+      <MovieInfoBar
+        time={movie.runtime}
+        budget={movie.budget}
+        revenue={movie.revenue}
+      />
+      <Grid header='Actors'>
+        {movie.actors.map(actor => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   );
 };
