ee194413ead1d0064d224bce734f0086c0badaa4
zigamak <zigamakinde@gmail.com>
2022-05-04 02:14:32 +0100

    Added Fetch Movie Hook

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+//Config
+import {IMAGE_BASE_URL, POSTER_SIZE} from '../config';
+//Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+//Hook
+import { useMovieFetch } from "../hooks/useMovieFetch";
+//Image
+import noImage from '../images/no_image.jpg';
 
-const Movie = () => <div>Movie</div>
+const Movie = () => {
+    const {movieId}= useParams();
+    const{state:movie,loading,error}=useMovieFetch(movieId)
+    console.log(movie);
+    return (
+        <>
+            <div>
+                Movies
+            </div>
+        </>
+    )
+}
 
 export default Movie
