eceda200bc4ed6e3a73a9d1a8a027df4ee830c0d
zigamak <zigamakinde@gmail.com>
2022-05-04 02:32:26 +0100

    Added Styles for the Breadcrumb

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,27 +1,29 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-//Config
-import {IMAGE_BASE_URL, POSTER_SIZE} from '../config';
-//Components
+import React from 'react';
+import { useParams } from 'react-router-dom';
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+// Components
 import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
-//Hook
-import { useMovieFetch } from "../hooks/useMovieFetch";
-//Image
-import noImage from '../images/no_image.jpg';
+// Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
+// Image
+import NoImage from '../images/no_image.jpg';
 
 const Movie = () => {
-    const {movieId}= useParams();
-    const{state:movie,loading,error}=useMovieFetch(movieId)
-    
-    if (loading) return <Spinner></Spinner>
-    if (error) return <div>Something went wrong ...</div>
-    return (
-        <>
-           <BreadCrumb movieTitle={movie.original_title} ></BreadCrumb>
-        </>
-    )
-}
+  const { movieId } = useParams();
 
-export default Movie
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  if (loading) return <Spinner />;
+  if (error) return <div>Something went wrong...</div>;
+
+  return (
+    <>
+      <BreadCrumb movieTitle={movie.original_title} />
+    </>
+  );
+};
+
+export default Movie;
