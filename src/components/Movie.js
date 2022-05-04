1114f5757997f44b851557f2cb0372c0f8157790
zigamak <zigamakinde@gmail.com>
2022-05-04 02:28:58 +0100

    Added Breadcrumb

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 //Config
 import {IMAGE_BASE_URL, POSTER_SIZE} from '../config';
 //Components
+import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
 //Hook
@@ -13,12 +14,12 @@ import noImage from '../images/no_image.jpg';
 const Movie = () => {
     const {movieId}= useParams();
     const{state:movie,loading,error}=useMovieFetch(movieId)
-    console.log(movie);
+    
+    if (loading) return <Spinner></Spinner>
+    if (error) return <div>Something went wrong ...</div>
     return (
         <>
-            <div>
-                Movies
-            </div>
+           <BreadCrumb movieTitle={movie.original_title} ></BreadCrumb>
         </>
     )
 }
