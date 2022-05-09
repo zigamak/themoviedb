78fd937b5738475cb7830a7148a41508545728a4
zigamak <zigamakinde@gmail.com>
2022-05-09 23:33:46 +0100

    Using class component in the search bar component

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,41 +1,46 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // Image
 import searchIcon from '../../images/search-icon.svg';
 // Styles
 import { Wrapper, Content } from './SearchBar.styles';
 
-const SearchBar = ({ setSearchTerm }) => {
-  const [state, setState] = useState('');
-  const initial = useRef(true);
+class SearchBar extends Component {
+  state = { value: '' };
+  timeout = null;
 
-  useEffect(() => {
-    if (initial.current) {
-      initial.current = false;
-      return;
+  componentDidUpdate(_prevProps, prevState) {
+    if (this.state.value !== prevState.value) {
+      const { setSearchTerm } = this.props;
+
+      clearTimeout(this.timeout);
+
+      this.timeout = setTimeout(() => {
+        const { value } = this.state;
+        setSearchTerm(value);
+      }, 500);
     }
+  }
 
-    const timer = setTimeout(() => {
-      setSearchTerm(state);
-    }, 500);
+  render() {
 
-    return () => clearTimeout(timer);
-  }, [setSearchTerm, state]);
+    const { value } = this.state;
 
-  return (
-    <Wrapper>
-      <Content>
-        <img src={searchIcon} alt='search-icon' />
-        <input
-          type='text'
-          placeholder='Search Movie'
-          onChange={event => setState(event.currentTarget.value)}
-          value={state}
-        />
-      </Content>
-    </Wrapper>
-  );
-};
+    return (
+      <Wrapper>
+        <Content>
+          <img src={searchIcon} alt='search-icon' />
+          <input
+            type='text'
+            placeholder='Search Movie'
+            onChange={event => this.setState({ value: event.currentTarget.value })}
+            value={value}
+          />
+        </Content>
+      </Wrapper>
+    );
+  }
+}
 
 SearchBar.propTypes = {
   callback: PropTypes.func
